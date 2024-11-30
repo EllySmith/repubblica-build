eab1479e185ed406216e3e8333fb24a14d56c688
Alexandra Adelson <adelson.alexandra@gmail.com>
2024-11-30 03:01:48 +0100

    image fix

--- src/Components/ArticleBox/BigArticle.js
+++ src/Components/ArticleBox/BigArticle.js
@@ -16,16 +16,17 @@ function BigArticle({ item }) {
   className="flex flex-col lg:flex-row gap-4 bg-white shadow-lg border-t-2 border-black overflow-hidden h-full transform transition-all duration-300 ease-in-out hover:z-50 hover:bg-gray-200 hover:border-black relative"
   onClick={() => handleModal(link)}
 >
-      <div className="w-full lg:w-1/3">
+<div className="w-full lg:w-1/3 h-48 sm:h-64 lg:h-auto">
         <picture>
           <source type="image/webp" srcSet={imageUrl} />
           <img
-            className="w-full h-full object-cover"
+            className="w-full h-full object-contain sm:object-cover"
             src={imageUrl}
             alt={title}
           />
         </picture>
       </div>
+
 
       <div className="flex flex-col justify-between p-4 w-full lg:w-2/3">
         <h2 className="text-xl font-bold mb-2">{body}</h2>
